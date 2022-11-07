11f3ad5d9d76f65fc367a31002052f470e1649c4
2130845h <114968658+2130845h@users.noreply.github.com>
2022-11-08 00:44:44 +0900

    section5-q1 change

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,19 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",100, 100, 200, 255, 200);
 }
 
-function balloon(t){
+function balloon(t, x, y, r, g, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
+  let wid = w + p * 2
+  let hei = h + p * 4
+  noStroke();
+  fill(r, g, b);
+  rect(x, y, wid, hei);
+  triangle(wid / 2 + x, hei + y, wid / 2 + x + 20 , hei + y , wid / 2 + x + 5, (hei + y) + 15); 
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  text(t, p + x, h + p + y);
 }
